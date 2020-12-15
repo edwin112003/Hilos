af3df989706ae836b14c2b4d8cb5950cbfd4d360
edwin112003 <edwinaguilar112003@gmail.com>
2020-12-15 14:49:38 -0600

    xd4

--- main.js
+++ main.js
@@ -1,12 +1,13 @@
 var express = require('express');
 var app = express();
+const path = require('path');
 //Creamos el servidor
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
 
 var messages = [];
 
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname,'public')));
 //cuando recibamos un get en la ruta raiz 
 //active la funcion
 
@@ -23,7 +24,7 @@ io.on('connection', function(socket){
     });
 });
 
-const port = process.env.PORT || 8080;
+var port = process.env.PORT || 8080; 
 //Prbamos que todo va bien xd
 server.listen(port, function(){
     console.log("Servidor corriendo en http://localhost:8080");
